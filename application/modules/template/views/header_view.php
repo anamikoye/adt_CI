448b48952bdd092aa5fb43009d3796d2a5314cdb
OmondiKevin <kecovhi09@gmail.com>
2016-09-09 16:02:32 +0300

    Fixed Bugs

--- application/modules/template/views/header_view.php
+++ application/modules/template/views/header_view.php
@@ -10,7 +10,6 @@
 <meta name="keywords" content="HTML, CSS, XML, XHTML, JavaScript">
 <meta name="author" content="Star Sarifi Tours">
 <meta name="Resource-type" content="Document">
- --><!-- Attaching the aadverntsafaris Icon logo -->
 <link rel="shortcut icon" type="img/x-icon" href="<?php echo base_url() .'assets/icons/afric.jpg'?>"/>
 <!-- dynamic files includes all the .js .css plugins-->
  	<?php      	
