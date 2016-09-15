1c4d4a2bb8d503e64f129cc9c3a5a834a9838c79
Brian Mwathi <mwathibrian7@gmail.com>
2016-09-15 12:04:12 +0300

    Added Sidebar Info

--- application/modules/patient/views/sidebar.php
+++ application/modules/patient/views/sidebar.php
@@ -44,7 +44,68 @@
 							</li>
 						</ul>	
 					</li>
-					<li class="top-menu-invisible">
+					<!-- !! Patients Zone Begin !! -->
+					<li>
+						<a href="#"><i class=""></i><span class="menu-item-parent">Patients</span></a>
+						<ul>
+							<li>
+							<a href="#"><span class="menu-item-parent">Add patients</span></a>
+							</li>
+							<li>
+							<a href="#"><span class="menu-item-parent">Active patients</span></a>
+							</li>
+							<li>
+							<a href="#"><span class="menu-item-parent">Inactive patients</span></a>
+							</li>
+							<li>
+							<a href="#"><span class="menu-item-parent">All patients</span></a>
+							</li>
+							<li>
+							<a href="#"><span class="menu-item-parent">Export patients master list</span></a>
+							</li>
+						</ul>
+					</li>
+					<!-- !! Patients Zone End !! -->
+					<!-- !! Inventory Zone Begin !! -->
+					<li>
+						<a href="#"><i class=""></i><span class="menu-item-parent">Inventory</span></a>
+						<ul>
+							<li>
+							<a href="#"><span class="menu-item-parent">Stock transactions</span></a>
+							<ul>
+								<li>
+								<a href="#"><span class="menu-item-parent">Receive/Issue</span></a>
+								</li>
+							</ul>
+							</li>			
+							<li>
+							<a href="#"><span class="menu-item-parent">Stores</span></a>
+							</li>
+						</ul>
+					</li>
+					<!-- !! Inventory Zone End !! -->
+					<!-- !! Inventory Zone Begin !! -->
+					<li>
+						<a href="#"><i class=""></i><span class="menu-item-parent">Orders</span></a>
+						<ul>
+							<li>
+							<a href="#"><span class="menu-item-parent">MyCDDRS</span></a>							
+							</li>
+							<li>
+							<a href="#"><span class="menu-item-parent">My maps</span></a>
+							</li>			
+							<li>
+							<a href="#"><span class="menu-item-parent">My templates</span></a>
+							</li>
+						</ul>
+					</li>
+					<!-- !! Inventory Zone End !! -->
+					<!-- 
+						PLEASE DO NOT TAMPER WITH THIS SECTION!
+						Brian Mwathi was here on 15th Sept 2016
+					-->
+
+					<!-- <li class="top-menu-invisible">
 						<a href="#"><i class="fa fa-lg fa-fw fa-cube txt-color-blue"></i> <span class="menu-item-parent">SmartAdmin Intel</span></a>
 						<ul>
 							<li class="">
@@ -60,8 +121,8 @@
 								<a href="ajax/applayout.html"><i class="fa fa-cube"></i> App Settings</a>
 							</li>
 						</ul>
-					</li>
-					<li>
+					</li> -->
+					<!-- <li>
 						<a href="ajax/inbox.html"><i class="fa fa-lg fa-fw fa-inbox"></i> <span class="menu-item-parent">Outlook</span> <span class="badge pull-right inbox-badge margin-right-13">14</span></a>
 						<ul>
 							<li>
@@ -80,8 +141,8 @@
 								<a href="ajax/email-template.html">Email Templates </a>
 							</li>
 						</ul>	
-					</li>
-					<li>
+					</li> -->
+					<!-- <li>
 						<a href="#"><i class="fa fa-lg fa-fw fa-bar-chart-o"></i> <span class="menu-item-parent">Graphs</span></a>
 						<ul>
 							<li>
@@ -245,11 +306,11 @@
 								</ul>
 							</li>
 						</ul>
-					</li>	
-					<li>
+					</li> -->	
+					<<!-- li>
 						<a href="ajax/widgets.html"><i class="fa fa-lg fa-fw fa-list-alt"></i> <span class="menu-item-parent">Widgets</span></a>
-					</li>
-					<li>
+					</li> -->
+					<!-- <li>
 						<a href="#"><i class="fa fa-lg fa-fw fa-cloud"><em>3</em></i> <span class="menu-item-parent">Cool Features!</span></a>
 						<ul>
 							<li>
@@ -333,147 +394,8 @@
 								<a href="ajax/blank_.html">Blank Page</a>
 							</li>
 						</ul>
-					</li>
-					<li class="chat-users top-menu-invisible">
-						<a href="#"><i class="fa fa-lg fa-fw fa-comment-o"><em class="bg-color-pink flash animated">!</em></i> <span class="menu-item-parent">Smart Chat API <sup>beta</sup></span></a>
-						<ul>
-							<li>
-								<!-- DISPLAY USERS -->
-								<div class="display-users">
-
-									<input class="form-control chat-user-filter" placeholder="Filter" type="text">
-									
-								  	<a href="#" class="usr" 
-									  	data-chat-id="cha1" 
-									  	data-chat-fname="Sadi" 
-									  	data-chat-lname="Orlaf" 
-									  	data-chat-status="busy" 
-									  	data-chat-alertmsg="Sadi Orlaf is in a meeting. Please do not disturb!" 
-									  	data-chat-alertshow="true" 
-									  	data-rel="popover-hover" 
-									  	data-placement="right" 
-									  	data-html="true" 
-									  	data-content="
-											<div class='usr-card'>
-												<img src="<php echo base_url().'assets/templates/img/avatars/5.png'?>" alt='Sadi Orlaf'>
-												<div class='usr-card-content'>
-													<h3>Sadi Orlaf</h3>
-													<p>Marketing Executive</p>
-												</div>
-											</div>
-										"> 
-									  	<i></i>Sadi Orlaf
-								  	</a>
-								  
-									<a href="#" class="usr" 
-										data-chat-id="cha2" 
-									  	data-chat-fname="Jessica" 
-									  	data-chat-lname="Dolof" 
-									  	data-chat-status="online" 
-									  	data-chat-alertmsg="" 
-									  	data-chat-alertshow="false" 
-									  	data-rel="popover-hover" 
-									  	data-placement="right" 
-									  	data-html="true" 
-									  	data-content="
-											<div class='usr-card'>
-												<img src='img/avatars/1.png' alt='Jessica Dolof'>
-												<div class='usr-card-content'>
-													<h3>Jessica Dolof</h3>
-													<p>Sales Administrator</p>
-												</div>
-											</div>
-										"> 
-									  	<i></i>Jessica Dolof
-									</a>
-								  
-									<a href="#" class="usr" 
-									  	data-chat-id="cha3" 
-									  	data-chat-fname="Zekarburg" 
-									  	data-chat-lname="Almandalie" 
-									  	data-chat-status="online" 
-									  	data-rel="popover-hover" 
-									  	data-placement="right" 
-									  	data-html="true" 
-									  	data-content="
-											<div class='usr-card'>
-												<img src='img/avatars/3.png' alt='Zekarburg Almandalie'>
-												<div class='usr-card-content'>
-													<h3>Zekarburg Almandalie</h3>
-													<p>Sales Admin</p>
-												</div>
-											</div>
-										"> 
-									  	<i></i>Zekarburg Almandalie
-									</a>
-								 
-									<a href="#" class="usr" 
-									  	data-chat-id="cha4" 
-									  	data-chat-fname="Barley" 
-									  	data-chat-lname="Krazurkth" 
-									  	data-chat-status="away" 
-									  	data-rel="popover-hover" 
-									  	data-placement="right" 
-									  	data-html="true" 
-									  	data-content="
-											<div class='usr-card'>
-												<img src='img/avatars/4.png' alt='Barley Krazurkth'>
-												<div class='usr-card-content'>
-													<h3>Barley Krazurkth</h3>
-													<p>Sales Director</p>
-												</div>
-											</div>
-										"> 
-									  	<i></i>Barley Krazurkth
-									</a>
-								  
-									<a href="#" class="usr offline" 
-									  	data-chat-id="cha5" 
-									  	data-chat-fname="Farhana" 
-									  	data-chat-lname="Amrin" 
-									  	data-chat-status="incognito" 
-									  	data-rel="popover-hover" 
-									  	data-placement="right" 
-									  	data-html="true" 
-									  	data-content="
-											<div class='usr-card'>
-												<img src='img/avatars/female.png' alt='Farhana Amrin'>
-												<div class='usr-card-content'>
-													<h3>Farhana Amrin</h3>
-													<p>Support Admin <small><i class='fa fa-music'></i> Playing Beethoven Classics</small></p>
-												</div>
-											</div>
-										"> 
-									  	<i></i>Farhana Amrin (offline)
-									</a>
-								  
-									<a href="#" class="usr offline" 
-										data-chat-id="cha6" 
-									  	data-chat-fname="Lezley" 
-									  	data-chat-lname="Jacob" 
-									  	data-chat-status="incognito" 
-									  	data-rel="popover-hover" 
-									  	data-placement="right" 
-									  	data-html="true" 
-									  	data-content="
-											<div class='usr-card'>
-												<img src='img/avatars/male.png' alt='Lezley Jacob'>
-												<div class='usr-card-content'>
-													<h3>Lezley Jacob</h3>
-													<p>Sales Director</p>
-												</div>
-											</div>
-										"> 
-									  	<i></i>Lezley Jacob (offline)
-									</a>
-									
-									<a href="ajax/chat.html" class="btn btn-xs btn-default btn-block sa-chat-learnmore-btn">About the API</a>
-
-								</div>
-								<!-- END DISPLAY USERS -->
-							</li>
-						</ul>	
-					</li>
+					</li> -->
+					
 				</ul>
 			</nav>
 			<a href="#ajax/difver.html" class="btn btn-primary nav-demo-btn"><i class="fa fa-info-circle"></i> SmartAdmin Package (187 MB)</a>
